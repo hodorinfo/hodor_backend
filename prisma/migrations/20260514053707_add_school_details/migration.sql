c8dc4ec870926871b71b4ae1f9703234402e1676
Sadikkhan02 <sadiksalmani2004@gmail.com>
2026-05-14 15:34:52 +0530

    refactor: remove redundant configuration file and associated logic

--- prisma/migrations/20260514053707_add_school_details/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Candidate" ADD COLUMN     "tenthBoard" TEXT,
-ADD COLUMN     "tenthPercentage" TEXT,
-ADD COLUMN     "tenthYear" TEXT,
-ADD COLUMN     "twelfthBoard" TEXT,
-ADD COLUMN     "twelfthPercentage" TEXT,
-ADD COLUMN     "twelfthStream" TEXT,
-ADD COLUMN     "twelfthYear" TEXT;
